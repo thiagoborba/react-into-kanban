67911d764a5e22c32b75d8b8a4afe3c86a0c0d2c
thiago <thborba1@gmail.com>
2024-03-04 20:48:21 -0300

    feat: basic card

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <div className="text-4xl text-red-500">Hello</div>;
+  const title = 'Do Market Research';
+  const id = 'BUS-1';
+  const points = 5;
+
+  return (
+    <div className="border rounded-lg px-2 m-2 bg-green-50">
+      <div className="text-base font-semibold py-2">{title}</div>
+      <div className="flex justify-between py-2 text-gray-700">
+        <div>{id}</div>
+        <div>{points}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
