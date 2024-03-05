35f410f44bd983332cd5d37792004b9526492cd4
thiago <thborba1@gmail.com>
2024-03-05 09:32:05 -0300

    feat: create tasks constant

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { TaskCard } from './Components';
-import { Task } from './Types';
+import { tasks } from './Constants';
 
 function App() {
-  const task: Task = {
-    title: 'Do Market Research',
-    id: 'BUS-1',
-    points: 5,
-  };
-
   return (
     <>
-      <TaskCard {...task} />
-      <TaskCard title="Competidor Analisys" id="BUS-2" />
-      <TaskCard title="Develop Business Strategy" id="BUS-3" points={8} />
-      <TaskCard title="Develop Marketing Strategy" id="BUS-4" points={5} />
+      {tasks.map((task) => (
+        <TaskCard key={task.id} {...task} />
+      ))}
     </>
   );
 }
---
+++ src/Constants/index.ts
@@ -0,0 +1,18 @@
+import { Task } from '../Types';
+
+export const tasks: Task[] = [
+  {
+    title: 'Do Market Research',
+    id: 'BUS-1',
+    points: 5,
+  },
+  {
+    title: 'Competidor Analisys',
+    id: 'BUS-2',
+  },
+  {
+    title: 'Develop Business Strategy',
+    id: 'BUS-3',
+    points: 8,
+  },
+];
