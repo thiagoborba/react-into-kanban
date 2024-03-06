6dbf2bd8e9f42e07c02193bf84e28be4f2a1adb7
thiago <thborba1@gmail.com>
2024-03-06 16:18:50 -0300

    feat: creates hover background effect

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import { Status, Task } from './Types';
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>(initialTasks);
+  const [currentlyHoveringOver, setCurrentlyHoveringOver] =
+    useState<Status | null>(null);
+
+  console.log(currentlyHoveringOver);
 
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status);
     return {
-      title: status,
+      status: status,
       tasks: tasksInColumn,
     };
   });
@@ -24,6 +28,9 @@ function App() {
 
   function handleDrop(e: React.DragEvent<HTMLDivElement>, status: Status) {
     e.preventDefault();
+
+    setCurrentlyHoveringOver(null);
+
     const taskId = e.dataTransfer.getData('id');
     const task = tasks.find((task) => task.id === taskId);
     if (task) {
@@ -31,23 +38,34 @@ function App() {
     }
   }
 
+  function handleDragEnter(status: Status) {
+    setCurrentlyHoveringOver(status);
+  }
+
   return (
     <div className="flex divide-x">
       {columns.map((column) => (
         <div
-          onDrop={(e) => handleDrop(e, column.title)}
+          onDrop={(e) => handleDrop(e, column.status)}
           onDragOver={(e) => e.preventDefault()}
+          onDragEnter={() => handleDragEnter(column.status)}
         >
           <div className=" flex justify-between text-3xl p-2 font-bold text-gray-500 ">
-            <h2 className="capitalize">{column.title}</h2>
+            <h2 className="capitalize">{column.status}</h2>
             <div>
               {column.tasks.reduce((acc, task) => acc + Number(task.points), 0)}
             </div>
           </div>
 
-          {column.tasks.map((task) => (
-            <TaskCard updateTask={updateTask} task={task} />
-          ))}
+          <div
+            className={`h-full ${
+              currentlyHoveringOver === column.status ? 'bg-gray-200' : ''
+            }`}
+          >
+            {column.tasks.map((task) => (
+              <TaskCard updateTask={updateTask} task={task} />
+            ))}
+          </div>
         </div>
       ))}
     </div>
